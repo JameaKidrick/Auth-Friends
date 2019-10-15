4b0a22e05f8b5f54ef875468af6ac0eda159b761
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2019-10-15 15:47:57 -0400

    added routes for login, home, and error pages

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+// COMPONENTS
 import LoginPage from './components/LoginPage'
+import HomePage from './components/HomePage'
+import ErrorPage from './components/ErrorPage'
 
 function App() {
   return (
-    <div className="App">
-      Hello World!
-      {/* <LoginPage /> */}
-
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path ='/api/login' component={LoginPage} />
+          <Route component={ErrorPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- friends/src/components/LoginPage.js
+++ friends/src/components/LoginPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoginPage = () => {
   
-  return(
+  return (
     <div>
       Hello LoginPage!
       <form>
